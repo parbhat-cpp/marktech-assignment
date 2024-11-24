c85d20ae5e6b3b9b42a31449de192ec2dcd448a5
Parbhat Sharma <83265789+parbhat-cpp@users.noreply.github.com>
2024-11-24 13:26:16 +0530

    Update config.ts

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,15 +1,3 @@
-import { resolve } from "path";
-import dotenv from "dotenv";
-
-const ENV_FILE_PATH = resolve(".env");
-
-const isEnvFound = dotenv.config({ path: ENV_FILE_PATH });
-
-// check if .env file exists
-if (isEnvFound.error) {
-    throw new Error("Cannot find .env file");
-}
-
 // setting default value if not provided
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 process.env.SERVER_PORT = process.env.SERVER_PORT || "5000";
